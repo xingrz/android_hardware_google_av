1784c109c90abadb9aaca61f0c66a9265237fe14
Praveen Chavan <pchavan@codeaurora.org>
2018-03-23 07:01:23 -0700

    codec2: Input buffer lifecycle hack
    Input buffer reference is released after work is queued to
    component (over HIDL). This makes the buffer-slot seem like it is
    available for the client, while the component still references
    the buffer.
    
    Keep local references to input buffers queued to the component till
    they are returned via workDone; track them via frame-id.
    
    Bug: 31973802
    Bug: 64121714
    Bug: 74006396
    Change-Id: I7994485899f227eea3b76925228c4113390c171e

--- media/sfplugin/CCodecBufferChannel.cpp
+++ media/sfplugin/CCodecBufferChannel.cpp
@@ -1060,6 +1060,10 @@ status_t CCodecBufferChannel::queueInputBufferInternal(const sp<MediaCodecBuffer
             return -ENOENT;
         }
         work->input.buffers.push_back(c2buffer);
+
+        // TODO: Use BufferPool
+        Mutexed<InputRefs>::Locked inputRefs(mInputRefs);
+        inputRefs->bufferRefs.emplace(work->input.ordinal.frameIndex.peekull(), c2buffer);
     }
     // TODO: fill info's
 
@@ -1428,6 +1432,16 @@ void CCodecBufferChannel::flush(const std::list<std::unique_ptr<C2Work>> &flushe
 }
 
 void CCodecBufferChannel::onWorkDone(const std::unique_ptr<C2Work> &work) {
+    if (work->input.buffers.size() == 1) {
+        // TODO: Use BufferPool
+        Mutexed<InputRefs>::Locked inputRefs(mInputRefs);
+        auto found = inputRefs->bufferRefs.find(work->input.ordinal.frameIndex.peekull());
+        if (found != inputRefs->bufferRefs.end()) {
+            ALOGV("onWorkdone: removing input ref for frame=%llu",
+                    work->input.ordinal.frameIndex.peekull());
+            inputRefs->bufferRefs.erase(found);
+        }
+    }
     if (work->result != C2_OK) {
         if (work->result == C2_NOT_FOUND) {
             // TODO: Define what flushed work's result is.
