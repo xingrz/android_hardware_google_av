2bb5906d38766a26b8ef5f80822b3e803be0c4ad
Wonsik Kim <wonsik@google.com>
2018-07-25 09:48:10 -0700

    faultinjection: initial revised API
    Bug: 80249011
    Test: make libc2_component_wrapper
    Change-Id: Ia3b5d23463237fe1d72448f5b369f6865b8e4b57

--- codec2/faultinjection/C2ComponentWrapper.h
+++ codec2/faultinjection/C2ComponentWrapper.h
@@ -17,8 +17,9 @@
 #ifndef C2_COMPONENT_WRAPPER_H_
 #define C2_COMPONENT_WRAPPER_H_
 
-#include <stdbool.h>
-#include <stdint.h>
+#include <C2Component.h>
+
+#include "SimpleMethodState.h"
 
 namespace android {
 
@@ -26,42 +27,40 @@ namespace android {
  * Creates a Wrapper around the class C2Component and its methods. The wrapper is used to
  * simulate errors in the android media components by fault injection technique.
  * This is done to check how the framework handles the error situation.
- *
  */
 class C2ComponentWrapper
         : public C2Component, public std::enable_shared_from_this<C2ComponentWrapper> {
 public:
-/**
- * Creates a wrapper around the listener class inside C2Component class.
- */
+    class Injecter {
+    public:
+        explicit Injecter(C2ComponentWrapper *thiz);
+
+        SimpleMethodState::Injecter start();
+    private:
+        C2ComponentWrapper *const mThiz;
+    };
+
+    /**
+     * A wrapper around the listener class inside C2Component class.
+     */
     class Listener : public C2Component::Listener {
     public:
         explicit Listener(const std::shared_ptr<C2Component::Listener> &listener);
         virtual ~Listener() = default;
 
-        enum FaultMode {
-            // run the method for infinite amount of time
-            IS_INFINITE,
-            // the status of the work is changed
-            IS_ALTERED
-        };
-
-        c2_status_t mAlteredListenerResult;
-        FaultMode mWorkDoneMode;
-        void setOnWorkDoneMode(Listener::FaultMode mode);
-        void setAlteredListenerResult(c2_status_t status);
         void onWorkDone_nb(std::weak_ptr<C2Component> component,
                            std::list<std::unique_ptr<C2Work>> workItems) override;
         void onTripped_nb(std::weak_ptr<C2Component> component,
                           std::vector<std::shared_ptr<C2SettingResult>> settingResult) override;
         void onError_nb(std::weak_ptr<C2Component> component, uint32_t errorCode) override;
- 
+
     private:
         std::shared_ptr<C2Component::Listener> mListener;
     };
 
     explicit C2ComponentWrapper(const std::shared_ptr<C2Component> &comp);
     virtual ~C2ComponentWrapper() = default;
+
     virtual c2_status_t setListener_vb(
             const std::shared_ptr<C2Component::Listener> &listener,
             c2_blocking_t mayBlock) override;
@@ -76,49 +75,13 @@ public:
     virtual c2_status_t release() override;
     virtual std::shared_ptr<C2ComponentInterface> intf() override;
 
-    enum FaultMode {
-        // work fine with no errors
-        WORK_OKAY,
-        // error with corrupt value
-        IS_CORRUPT,
-        // error with timed out component
-        IS_TIMED_OUT,
-        // run the method for infinite amount of time
-        IS_INFINITE,
-        // error with handling memory
-        HAS_NO_MEMORY,
-        // bad internal state error
-        IS_BAD_STATE
-    };
-
-    enum FlushDrainFaultMode {
-        // run the method for infinite amount of time
-        IS_HANG,
-        // the status of the work is changed
-        IS_ALTERED
-    };
-
-    FlushDrainFaultMode mFlushMode;
-    FlushDrainFaultMode mDrainMode;
-    FaultMode mStartMode;
-    FaultMode mStopMode;
-    FaultMode mResetMode;
-    FaultMode mReleaseMode;
-    c2_status_t mAlteredResult;
-
-    void setStartMode(FaultMode mode);
-    void setStopMode(FaultMode mode);
-    void setResetMode(FaultMode mode);
-    void setReleaseMode(FaultMode mode);
-    void setFlushMode(FlushDrainFaultMode mode);
-    void setDrainMode(FlushDrainFaultMode mode);
-    void setAlteredDrainResult(c2_status_t status);
-    void setAlteredFlushResult(c2_status_t status);
+    Injecter inject();
 
 private:
-    c2_status_t switchMode(FaultMode mode, std::function<c2_status_t()> func);
     std::shared_ptr<Listener> mListener;
     std::shared_ptr<C2Component> mComp;
+
+    SimpleMethodState mStartState;
 };
 
 }  // namespace android
