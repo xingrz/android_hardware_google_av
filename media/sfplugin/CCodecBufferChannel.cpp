0a80bab61f17b49551be6e2a8e23b64e583c46ed
Wonsik Kim <wonsik@google.com>
2018-11-20 16:45:18 -0800

    CCodec: alter input surface start/release sequence
    Bug: 116174324
    Bug: 121112375
    Bug: 121242363
    Test: bug repro steps
    Test: atest CtsCameraTestCases:RecordingTest#testAbandonedHighSpeedRequest
    Test: atest CtsMediaTestCases:MediaCodecTest#testReconfigureWithoutSurface
    Change-Id: I5904aad162788bfe3f0de8c8becefac2d53aaf71
    (cherry picked from commit 211b74ac8d0312830d096b91e369465107dcbfe3)

--- media/sfplugin/CCodecBufferChannel.cpp
+++ media/sfplugin/CCodecBufferChannel.cpp
@@ -2256,7 +2256,6 @@ void CCodecBufferChannel::stop() {
     mSync.stop();
     mFirstValidFrameIndex = mFrameIndex.load();
     if (mInputSurface != nullptr) {
-        mInputSurface->disconnect();
         mInputSurface.reset();
     }
 }
