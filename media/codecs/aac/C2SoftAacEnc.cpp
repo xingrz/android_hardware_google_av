6a42bc8be07a8a5561c1ef504694033859c3d964
Wonsik Kim <wonsik@google.com>
2019-04-01 13:58:48 -0700

    aac: set initial timestamp to zero
    Bug: 127405861
    Test: bug repro steps
    Change-Id: Id080eeb2345c19651329da3a6b7d7928a9aa3463
    (cherry picked from commit 19da254e6a07c909241db2ff2cd8338059bf7745)

--- media/codecs/aac/C2SoftAacEnc.cpp
+++ media/codecs/aac/C2SoftAacEnc.cpp
@@ -153,8 +153,9 @@ C2SoftAacEnc::C2SoftAacEnc(
       mSentCodecSpecificData(false),
       mInputTimeSet(false),
       mInputSize(0),
-      mInputTimeUs(-1ll),
-      mSignalledError(false) {
+      mInputTimeUs(0),
+      mSignalledError(false),
+      mOutIndex(0u) {
 }
 
 C2SoftAacEnc::~C2SoftAacEnc() {
@@ -178,7 +179,7 @@ c2_status_t C2SoftAacEnc::onStop() {
     mSentCodecSpecificData = false;
     mInputTimeSet = false;
     mInputSize = 0u;
-    mInputTimeUs = -1ll;
+    mInputTimeUs = 0;
     mSignalledError = false;
     return C2_OK;
 }
@@ -196,6 +197,7 @@ c2_status_t C2SoftAacEnc::onFlush_sm() {
     mSentCodecSpecificData = false;
     mInputTimeSet = false;
     mInputSize = 0u;
+    mInputTimeUs = 0;
     return C2_OK;
 }
 
@@ -500,6 +502,7 @@ c2_status_t C2SoftAacEnc::drain(
     mSentCodecSpecificData = false;
     mInputTimeSet = false;
     mInputSize = 0u;
+    mInputTimeUs = 0;
 
     // TODO: we don't have any pending work at this time to drain.
     return C2_OK;
